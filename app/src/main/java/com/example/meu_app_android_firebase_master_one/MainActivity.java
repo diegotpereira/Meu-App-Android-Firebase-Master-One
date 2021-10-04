c739424ce10529594eb51797c2ee298dafd397e5
Diego <diegoteixeirapereira@hotmail.com>
2021-10-04 09:54:10 -0300

    Criação do app crud firebase

---
+++ app/src/main/java/com/example/meu_app_android_firebase_master_one/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.meu_app_android_firebase_master_one;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
